fb886f2e69d841f6df3568f2b838ad9a49aff638
nyorain <nyorain@gmail.com>
2015-07-26 18:55:41 +0200

    added general header file

---
+++ src/nyutil/nyutil.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <nyutil/vec.hpp>
+#include <nyutil/argParser.hpp>
+#include <nyutil/refVec.hpp>
+#include <nyutil/mat.hpp>
+#include <nyutil/thread.hpp>
+#include <nyutil/nonCopyable.hpp>
+#include <nyutil/timer.hpp>
+#include <nyutil/module.hpp>
+#include <nyutil/callback.hpp>
+#include <nyutil/rect.hpp>
