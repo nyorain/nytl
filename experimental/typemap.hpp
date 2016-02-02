b5cd27f67227df9b12c1ccc1c863894398cea62a
nyorain <nyorain@gmail.com>
2016-02-02 18:48:11 +0100

    reworkd nytl::Typemap

--- experimental/typemap.hpp
+++ experimental/typemap.hpp
@@ -22,6 +22,9 @@
  * SOFTWARE.
  */
 
+///\file
+///Provides the Typemap template class that allows psuedo-dynamic object creation/storage.
+
 #pragma once
 
 #include <nytl/make_unique.hpp>
@@ -31,6 +34,7 @@
 #include <typeinfo>
 #include <memory>
 #include <type_traits>
+#include <stdexcept>
 
 namespace nytl
 {
@@ -38,6 +42,7 @@ namespace nytl
 namespace detail
 {
 
+//make_unique_wrapper, returns a unique_ptr for usual types and a void* for void type
 template<typename Base, typename T, typename... Args> struct make_unique_wrapper
 {
     static std::unique_ptr<Base> call(Args... args) { return make_unique<T>(args...); };
@@ -51,55 +56,71 @@ template<typename T, typename... Args> struct make_unique_wrapper<void, T, Args.
 }
 
 
-//typemap
-template<typename Identifier, typename Base = void, typename... CArgs>
-class typemap
+///\ingroup utility
+///Can be thought of as map (like std::map) that holds pair of identifiers and types.
+///Internally just uses type erasure to "store" its types.
+///Therefore there are just a small set of operations that can then be done with those
+///"stored" types, like e.g. creating them from identifier or std::type_info or receive the 
+///type_info of the stored type by identifier.
+///\tparam I Identifier type for the types (usually std::string)
+///\tparam B Base type of all stored types. If there is none, use void (Created objects will then be
+///passed as void* objects). Defaulted to void
+///\tparam CA Additional construction args an objects needs on creation.
+template<typename I, typename B = void, typename... CArgs>
+class Typemap
 {
 public:
-    using ptr_type = typename std::conditional<std::is_same<Base, void>::value, void*, std::unique_ptr<Base>>::type;
+	using Base = B;
+	using Identifier = I;
+	using Factory = detail::make_unique_wrapper<B, CArgs...>;
+    using Pointer = typename 
+		std::conditional<std::is_same<Base, void>::value, void*, std::unique_ptr<Base>>::type;
 
     //base
-    struct typeBase
+    struct TypeBase
     {
     public:
-        virtual ~typeBase() = default;
-        virtual ptr_type create(CArgs... args) const = 0;
-        virtual const std::type_info& getTypeInfo() const = 0;
+        virtual ~TypeBase() = default;
+        virtual Pointer create(CArgs&&... args) const = 0;
+        virtual const std::type_info& typeInfo() const = 0;
     };
 
     //impl
     template<typename T>
-    struct typeImpl : typeBase
+    struct TypeImpl : TypeBase
     {
     public:
-        virtual ~typeImpl() = default;
-        virtual ptr_type create(CArgs... args) const override
-            { return detail::make_unique_wrapper<Base, T, CArgs...>::call(args...); }
-        virtual const std::type_info& getTypeInfo() const override { return typeid(T); };
+        virtual ~TypeImpl() = default;
+        virtual Pointer create(CArgs&&... args) const override
+            { return Factory::call(std::forward<CArgs>(args)...); }
+        virtual const std::type_info& typeInfo() const override { return typeid(T); };
     };
 
-    using map_type = std::unordered_map<Identifier, const typeBase*>;
-    using iterator = typename map_type::iterator;
-    using const_iterator = typename map_type::const_iterator;
+    using MapType = std::unordered_map<Identifier, std::unique_ptr<const TypeBase>>;
+    using Iterator = typename MapType::iterator;
+    using ConstIterator = typename MapType::const_iterator;
+
+	//stl conformance
+	using iterator = Iterator;
+	using const_iterator = ConstIterator;
+	using pointer = Pointer;
 
 protected:
-   map_type types_; //types stored
+   MapType types_; //types stored
 
 public:
-    //get
-    //id
-    iterator getByIdentifier(const Identifier& id){ return types_.find(id); }
-    const_iterator getByIdentifier(const Identifier& id) const { return types_.find(id); }
+    Iterator typeIterator(const Identifier& id){ return types_.find(id); }
+    ConstIterator typeIterator(const Identifier& id) const { return types_.find(id); }
 
     //typeinfo
-    iterator getByTypeInfo(const std::type_info& id)
+    Iterator typeIterator(const std::type_info& id)
     {
         auto it = types_.begin();
         for(; it != types_.end(); ++it)
             if(it->second->getTypeInfo() == id) break;
         return it;
     }
-    const_iterator getByTypeInfo(const std::type_info& id) const
+    ConstIterator typeIterator(const std::type_info& id) const
     {
         auto it = types_.cbegin();
         for(; it != types_.cend(); ++it)
@@ -108,75 +129,125 @@ public:
     }
 
     //entry
-    iterator getByEntry(const Identifier& id, const std::type_info& typeID)
-        { auto it = getByTypeInfo(typeID); if(it != types_.end() && it->first == id) return it; return types_.end(); }
-    const_iterator getByEntry(const Identifier& id, const std::type_info& typeID) const
-        { auto it = getByTypeInfo(typeID); if(it != types_.cend() && it->first == id) return it; return types_.cend(); }
+    Iterator typeIterator(const Identifier& id, const std::type_info& typeID)
+    {
+		auto it = typeIterator(typeID); 
+		if(it != types_.end() && it->first == id) 
+			return it; 
+		return types_.end(); 
+	}
+    ConstIterator typeIterator(const Identifier& id, const std::type_info& typeID) const
+    {
+	   	auto it = typeIterator(typeID); 
+		if(it != types_.cend() && it->first == id) 
+			return it; 
+		return types_.cend(); 
+	}
 
-    bool valid(const const_iterator& it) const { return it != types_.end(); }
-    bool valid(const iterator& it) const { return it != types_.end(); }
+    bool valid(const ConstIterator& it) const { return it != types_.end(); }
 
 public:
-    ~typemap(){ for(auto& val : types_) delete val.second; }
-
     template<typename T> std::size_t registerType(const Identifier& id)
     {
-        types_[id] = new typeImpl<T>();
+        types_[id] = new TypeImpl<T>();
         return types_.size();
     }
 
-    bool removeIdentifier(const Identifier& id)
+    bool remove(const Identifier& id)
     {
-        auto it = getByIdentifier(id);
-        if(it != types_.cend())
+        auto it = typeIterator(id);
+        if(valid(types_.cend()))
         {
             types_.erase(it);
             return 1;
         }
         return 0;
     }
-    template<typename T> bool removeType(const T& obj = T{})
+    template<typename T> bool remove(const T& obj = {})
     {
-        auto it = getByTypeInfo(typeid(obj));
-        if(it != types_.cend())
+        auto it = typeIterator(typeid(obj));
+        if(valid(types_.cend()))
         {
             types_.erase(it);
             return 1;
         }
         return 0;
     }
-    template<typename T> bool removeEntry(const Identifier& id, const T& obj = T{})
+    template<typename T> bool remove(const Identifier& id, const std::type_info& info)
     {
-        auto it = getByEntry(id, typeid(obj));
-        if(it != types_.cend())
+        auto it = typeIterator(id, info);
+        if(valid(types_.cend()))
         {
             types_.erase(it);
             return 1;
         }
         return 0;
     }
+    template<typename T> bool remove(const Identifier& id, const T& obj = T{})
+    {
+        return remove(id, typeid(obj));
+    }
 
-    bool identifierExists(const Identifier& id) const { return getByIdentifier(id) != types_.cend(); }
-    bool typeExists( const std::type_info& typeID) const { return getByTypeInfo(typeID) != types_.cend(); }
-    template<typename T> bool typeExists(const T& obj = T{}) const { return getByTypeInfo(typeid(obj)) != types_.cend(); }
-    template<typename T> bool entryExists(const Identifier& id, const T& obj = T{}) const { return getByEntry(id, typeid(obj)) != types_.cend(); }
-    bool entryExists(const Identifier& id, const std::type_info& typeID) const { return getByEntry(id, typeID) != types_.cend(); }
+    bool exists(const Identifier& id) const 
+		{ return typeIterator(id) != types_.cend(); }
+    bool exists( const std::type_info& typeID) const 
+		{ return typeIterator(typeID) != types_.cend(); }
+    template<typename T> bool exists(const T& obj = T{}) const 
+		{ return typeIterator(typeid(obj)) != types_.cend(); }
+    template<typename T> bool exists(const Identifier& id, const T& obj = T{}) const 
+		{ return typeIterator(id, typeid(obj)) != types_.cend(); }
+    bool eists(const Identifier& id, const std::type_info& typeID) const 
+		{ return typeIterator(id, typeID) != types_.cend(); }
 
-    ptr_type createObject(const Identifier& id, CArgs... args) const
-        { auto it = getByIdentifier(id); if(it != types_.cend()) return it->second->create(args...); return nullptr; }
-    ptr_type createObject(const std::type_info& id, CArgs... args) const
-        { auto it = getByTypeInfo(id); if(it != types_.cend()) return it->second->create(args...); return nullptr; }
+	///{
+	///Creates a object for the given type identifier or info with the given construction arguments.
+	///\return A unique_ptr of Base type (or void* if Base is void) or nullptr if the object could 
+	///not be constructed.
+    Pointer create(const Identifier& id, CArgs&&... args) const
+    { 
+		auto it = typeIterator(id); 
+		if(it != types_.cend()) 
+			return it->second->create(std::forward<CArgs>(args)...); 
+		return nullptr; 
+	}
+    Pointer create(const std::type_info& id, CArgs&&... args) const
+    { 
+		auto it = typeIterator(id); 
+		if(it != types_.cend()) 
+			return it->second->create(std::forward<CArgs>(args)...); 
+		return nullptr; 
+	}
+	///}
 
-    const Identifier& getID(const std::type_info& typeID) const { auto it = getByTypeInfo(typeID); if(it != types_.cend()) return it->first; return Identifier{}; }
-    template<typename T> const Identifier& getID(const T& obj = T{}) const { return getID(typeid(obj)); }
+	///\exception std::invalid_argument if there is no entry with the given type_info 
+    const Identifier& id(const std::type_info& typeID) const 
+	{ 
+		auto it = typeIterator(typeID); 
+		if(it != types_.cend()) 
+			return it->first; 
 
-    const std::type_info& getTypeInfo(const Identifier& id) const { if(typeExists(id)) return types_[id].getTypeInfo(); return typeid(void); }
-    const std::unordered_map<Identifier, const typeBase*>& getTypes() const { return types_; }
+		throw std::invalid_argument("nytl::Typemap::id: no entry with the given type_info");
+	}
+
+	///\exception std::invalid_argument if there is no entry for the given type
+    template<typename T> const Identifier& id(const T& obj = T{}) const 
+		{ return id(typeid(obj)); }
+
+	///\exception std::invalid_argument if there is no entry for the given identifier
+    const std::type_info& typeInfo(const Identifier& id) const 
+	{ 
+		if(typeExists(id)) 
+			return types_[id].typeInfo(); 
+		
+		throw std::invalid_argument("nytl::Typemap::typeInfo: no entry with the given id");
+	}
+    const std::unordered_map<Identifier, std::unique_ptr<const TypeBase>>& types() const 
+		{ return types_; }
 };
 
 //registerFunc
 template<typename T, typename Identifier, typename Base>
-unsigned int registerType(typemap<Identifier, Base>& m, const Identifier& id)
+unsigned int registerType(Typemap<Identifier, Base>& m, const Identifier& id)
 {
     return m.template registerType<T>(id);
 }
