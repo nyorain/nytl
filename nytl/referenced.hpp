df2d826cdfc8b04966d899d1e3d8403bf2475ba6
nyorain <nyorain@gmail.com>
2016-06-23 20:20:12 +0200

    fixes referenced bug

--- nytl/referenced.hpp
+++ nytl/referenced.hpp
@@ -45,9 +45,22 @@ protected:
 	virtual ~Referenced() = default;
 
 public:
-	void ref() const { referenceCount_++; }
-	void unref() const { referenceCount_--; if(referenceCount_ == 0) delete this; }
-	void unrefNodelete() const { referenceCount_--; }
+	auto ref() const { return ++referenceCount_; }
+	auto unref() const
+	{
+		auto cpy = referenceCount_.load();
+
+		while(true)
+		{
+			if(std::atomic_compare_exchange_weak(&referenceCount_, &cpy, cpy - 1)) break;
+			cpy = referenceCount_.load(); //reload if failed
+		}
+
+		//cpy should NEVER be 0 but who knows...
+		if(cpy <= 1) delete this;
+		return cpy > 0 ? cpy - 1 : 0;
+	}
+	auto unrefNodelete() const { return --referenceCount_; }
 
 	unsigned int referenceCount() const { return referenceCount_.load(); }
 };
