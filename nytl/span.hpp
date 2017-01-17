b7948d12f77c4f16b6296faec16139a066fe63e8
nyorain <nyorain@users.noreply.github.com>
2017-01-17 19:27:45 +0100

    Update span.hpp

--- nytl/span.hpp
+++ nytl/span.hpp
@@ -180,8 +180,7 @@ struct SpanStorage<const T, 0> {
 	}
 	constexpr SpanStorage(const T& ref, std::size_t size = 1) : SpanStorage(&ref, size) {}
 	template<std::size_t S> constexpr SpanStorage(T (&arr)[S]) : SpanStorage(arr, S) {}
-	constexpr SpanStorage(const std::initializer_list<T>& l)
-		: SpanStorage(l.begin(), l.size()) {}
+	constexpr SpanStorage(const std::initializer_list<T>& l) : SpanStorage(l.begin(), l.size()) {}
 
 	const T* data_ {};
 	std::size_t size_ {};
